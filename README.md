9c6c2ef003aa48a52c3f353e852537dbd50305ed
duongvinhloc <duongvinhloc1@gmail.com>
2023-09-18 00:15:57 +0900

    Create vue example

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # myVueApp
 https://qiita.com/minato-naka/items/2d2def4d66ec88dc3ca2
+
+
+# Error
+## Laravelで"The Mix manifest does not exist"が出た時の対処法
+https://qiita.com/keiswe/items/c52c38b69f77cfe1f3a4 
+npm install
+npm run dev 
--- vue-laravel-spa/resources/js/app.js
+++ vue-laravel-spa/resources/js/app.js
@@ -6,7 +6,13 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue').default;
+window.Vue = require('vue').default
+import VueRouter from 'vue-router';
+import HeaderComponent from "./components/HeaderComponent";
+import TaskListComponent from "./components/TaskListComponent";
+import TaskShowComponent from "./components/TaskShowComponent";
+import TaskCreateComponent from "./components/TaskCreateComponent";
+import TaskEditComponent from "./components/TaskEditComponent";
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,6 +26,7 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-component', HeaderComponent);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +34,38 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/tasks',
+            name: 'task.list',
+            component: TaskListComponent
+        },
+        {
+            path: '/tasks/create',
+            name: 'task.create',
+            component: TaskCreateComponent
+        },
+        {
+            path: '/tasks/:taskId',
+            name: 'task.show',
+            component: TaskShowComponent,
+            props: true
+        },
+        {
+            path: '/tasks/:taskId/edit',
+            name: 'task.edit',
+            component: TaskEditComponent,
+            props: true
+        },
+    ]
+});
+
+
 const app = new Vue({
     el: '#app',
+    router
 });
